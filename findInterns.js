9a704231f34fa0e40de6154e2dbb8b48a6da4e84
Sahmie Nwoko <sahmienwoko@gmail.com>
2020-04-11 20:34:19 +0100

    export movies collection

---
+++ findInterns.js
@@ -0,0 +1,4 @@
+let MongoClient = require('mongodb').MongoClient;
+let url = "mongodb://localhost:27017/";
+let collection = require('./interns')
+
--- interns.js
+++ interns.js
@@ -1,17 +1,23 @@
-
 let MongoClient = require('mongodb').MongoClient;
 let url = "mongodb://localhost:27017/";
 
-MongoClient.connect(url,(err, db) => {
+const collection = (err, db) => {
   if (err) throw err;
   let dbo = db.db("moviesdb");
   let movieList = [
-    {movie: "The Banker", year: "2020", rating: 8},  {movie: "Bad Boys", year: "2020", rating: 7}, {movie: "The Hunt", year: "2020", rating: 7}, {movie: "Bloodshot", year: "2020", rating: 7.5}, {movie: "First Cow", year: "2020", rating: 6.5}
+    {movie: "The Banker", year: "2020", rating: 8},
+    {movie: "Bad Boys", year: "2020", rating: 7}, 
+    {movie: "The Hunt", year: "2020", rating: 7}, 
+    {movie: "Bloodshot", year: "2020", rating: 7.5}, 
+    {movie: "First Cow", year: "2020", rating: 6.5}
   ];
-  dbo.collection("myMovies").insertMany(movieList, (err, result) => {
+   dbo.collection("myMovies").insertMany(movieList, (err, result) => {
     if (err) throw err;
     console.log(result);
     
     db.close();
   });
-}); 
+}; 
+MongoClient.connect(url, collection)
+
+module.exports = collection;
