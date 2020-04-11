b8fbed6b01904e29e4562022d2e49ee21dc049ff
Sahmie Nwoko <sahmienwoko@gmail.com>
2020-04-11 17:46:14 +0100

    add intern image to images folder

--- interns.js
+++ interns.js
@@ -8,9 +8,9 @@ MongoClient.connect(url,(err, db) => {
   let movieList = [
     {movie: "The Banker", year: "2020", rating: 8},  {movie: "Bad Boys", year: "2020", rating: 7}, {movie: "The Hunt", year: "2020", rating: 7}, {movie: "Bloodshot", year: "2020", rating: 7.5}, {movie: "First Cow", year: "2020", rating: 6.5}
   ];
-  dbo.collection("myMovies").insertMany(movieList, (err, res) => {
+  dbo.collection("myMovies").insertMany(movieList, (err, result) => {
     if (err) throw err;
-    console.log(res);
+    console.log(result);
     
     db.close();
   });
