f56536fe48de32e4eb2d162794eab899a1fe107b
Sahmie Nwoko <sahmienwoko@gmail.com>
2020-04-11 17:28:03 +0100

    create myMovies collection and inserted movies

--- interns.js
+++ interns.js
@@ -2,12 +2,16 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(url,(err, db) => {
   if (err) throw err;
-  var dbo = db.db("mydb");
-  dbo.createCollection("interns", function(err, res) {
+  let dbo = db.db("mydb");
+  let movieList = [
+    {movie: "The Banker", year: "2020", rating: 8},  {movie: "Bad Boys", year: "2020", rating: 7}, {movie: "The Hunt", year: "2020", rating: 7}, {movie: "Bloodshot", year: "2020", rating: 7.5}, {movie: "First Cow", year: "2020", rating: 6.5}
+  ];
+  dbo.collection("myMovies").insertMany(movieList, (err, res) => {
     if (err) throw err;
-    console.log("Interns collection created!");
+    console.log(res);
+    
     db.close();
   });
 }); 
