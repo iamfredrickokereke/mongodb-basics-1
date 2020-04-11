31eef325368b0cffd7da5cd07890a5d61822f1df
Sahmie Nwoko <sahmienwoko@gmail.com>
2020-04-11 19:07:41 +0100

    change var to let

--- interns.js
+++ interns.js
@@ -1,10 +1,10 @@
 
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+let MongoClient = require('mongodb').MongoClient;
+let url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url,(err, db) => {
   if (err) throw err;
-  let dbo = db.db("mydb");
+  let dbo = db.db("moviesdb");
   let movieList = [
     {movie: "The Banker", year: "2020", rating: 8},  {movie: "Bad Boys", year: "2020", rating: 7}, {movie: "The Hunt", year: "2020", rating: 7}, {movie: "Bloodshot", year: "2020", rating: 7.5}, {movie: "First Cow", year: "2020", rating: 6.5}
   ];
