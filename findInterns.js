5e0ef70fbc781fef2bcd69a0526f466022b82dc7
Sahmie Nwoko <sahmienwoko@gmail.com>
2020-04-12 19:32:31 +0100

    solved movie list task

--- findInterns.js
+++ findInterns.js
@@ -1,4 +1,27 @@
 let MongoClient = require('mongodb').MongoClient;
 let url = "mongodb://localhost:27017/";
-let collection = require('./interns')
+// let collections = require('./interns')
 
+MongoClient.connect(url, (err, db) => {
+  if (err) throw err;
+  let dbo = db.db("moviesdb");
+  let movieCollections = dbo.collection("myMovies");
+  movieCollections.findOne({}, (err, result)=>{
+    if (err) throw err;
+    console.log(result);
+    db.close()  
+  })
+
+  movieCollections.find({rating:7}).toArray( (err, result)=>{
+    if (err) throw err;
+    console.log(result);
+    db.close(); 
+  })
+  
+  movieCollections.find({}, { projection: {_id: 0, movie: 1}}).toArray( (err, result)=>{
+    if (err) throw err;
+    console.log(result);
+    db.close(); 
+  })
+ 
+});
